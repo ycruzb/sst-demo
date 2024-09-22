8dcd35d1ab6bf1a80e90a18a55e126ba18a94061
ycruzb <ycruzb@gmail.com>
2024-09-22 12:13:59 +0200

    add storage

--- infra/storage.ts
+++ infra/storage.ts
@@ -1,1 +1,9 @@
 export const bucket = new sst.aws.Bucket('Uploads');
+
+export const table = new sst.aws.Dynamo('Notes', {
+  fields: {
+    userId: 'string',
+    noteId: 'string',
+  },
+  primaryIndex: { hashKey: 'userId', rangeKey: 'noteId' },
+});
